fa5c728193fa5db9f8e1452c06454eb9110f181b
Ahmet-Acikgozz <aackgz08@gmail.com>
2025-12-15 17:05:40 +0300

    Rail Fence ​,Rotate Cipher,​,Columnar Transposition​,Hill Cipher eklendi

--- ServerApp/ServerApp/ServerForm.Designer.cs
+++ ServerApp/ServerApp/ServerForm.Designer.cs
@@ -2,7 +2,136 @@
 {
     partial class ServerForm
     {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.btnStopServer = new System.Windows.Forms.Button();
+            this.lblIPPort = new System.Windows.Forms.Label();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(12, 12);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(120, 30);
+            this.btnStartServer.TabIndex = 0;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // btnStopServer
+            // 
+            this.btnStopServer.Location = new System.Drawing.Point(140, 12);
+            this.btnStopServer.Name = "btnStopServer";
+            this.btnStopServer.Size = new System.Drawing.Size(120, 30);
+            this.btnStopServer.TabIndex = 1;
+            this.btnStopServer.Text = "Stop Server";
+            this.btnStopServer.UseVisualStyleBackColor = true;
+            this.btnStopServer.Click += new System.EventHandler(this.btnStopServer_Click);
+            // 
+            // lblIPPort
+            // 
+            this.lblIPPort.AutoSize = true;
+            this.lblIPPort.Location = new System.Drawing.Point(12, 55);
+            this.lblIPPort.Name = "lblIPPort";
+            this.lblIPPort.Size = new System.Drawing.Size(128, 15);
+            this.lblIPPort.TabIndex = 2;
+            this.lblIPPort.Text = "Server IP: 127.0.0.1 | ...";
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.ItemHeight = 15;
+            this.listBoxLog.Location = new System.Drawing.Point(12, 80);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(460, 349);
+            this.listBoxLog.TabIndex = 3;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(301, 15);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 23);
+            this.txtIP.TabIndex = 4;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(450, 15);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(60, 23);
+            this.txtPort.TabIndex = 5;
+            this.txtPort.Text = "9000";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(275, 18);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(20, 15);
+            this.lblIP.TabIndex = 6;
+            this.lblIP.Text = "IP:";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(412, 18);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(32, 15);
+            this.lblPort.TabIndex = 7;
+            this.lblPort.Text = "Port:";
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 450);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.lblIPPort);
+            this.Controls.Add(this.btnStopServer);
+            this.Controls.Add(this.btnStartServer);
+            this.Name = "ServerForm";
+            this.Text = "Kripto Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
         private System.Windows.Forms.Button btnStartServer;
         private System.Windows.Forms.Button btnStopServer;
         private System.Windows.Forms.Label lblIPPort;
@@ -11,98 +140,5 @@
         private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.Label lblIP;
         private System.Windows.Forms.Label lblPort;
-
-        private void InitializeComponent()
-        {
-            btnStartServer = new Button();
-            btnStopServer = new Button();
-            lblIPPort = new Label();
-            listBoxLog = new ListBox();
-            txtIP = new TextBox();
-            txtPort = new TextBox();
-            lblIP = new Label();
-            lblPort = new Label();
-            SuspendLayout();
-
-
-            
-            btnStartServer.Location = new Point(12, 12);
-            btnStartServer.Name = "btnStartServer";
-            btnStartServer.Size = new Size(120, 30);
-            btnStartServer.TabIndex = 0;
-            btnStartServer.Text = "Start Server";
-            btnStartServer.Click += btnStartServer_Click;
-
-
-            
-            btnStopServer.Location = new Point(140, 12);
-            btnStopServer.Name = "btnStopServer";
-            btnStopServer.Size = new Size(120, 30);
-            btnStopServer.TabIndex = 1;
-            btnStopServer.Text = "Stop Server";
-            btnStopServer.Click += btnStopServer_Click;
-
-
-            lblIPPort.Location = new Point(12, 50);
-            lblIPPort.Name = "lblIPPort";
-            lblIPPort.Size = new Size(400, 20);
-            lblIPPort.TabIndex = 2;
-            lblIPPort.Text = "Server IP: 127.0.0.1 | Port: 9000";
-
-
-            
-            listBoxLog.Location = new Point(12, 80);
-            listBoxLog.Name = "listBoxLog";
-            listBoxLog.Size = new Size(460, 344);
-            listBoxLog.TabIndex = 3;
-
-
-            
-            txtIP.Location = new Point(301, 15);
-            txtIP.Name = "txtIP";
-            txtIP.Size = new Size(100, 27);
-            txtIP.TabIndex = 4;
-            txtIP.Text = "127.0.0.1";
-
-
-             
-            txtPort.Location = new Point(450, 15);
-            txtPort.Name = "txtPort";
-            txtPort.Size = new Size(60, 27);
-            txtPort.TabIndex = 5;
-            txtPort.Text = "9000";
-
-
-             
-            lblIP.Location = new Point(265, 18);
-            lblIP.Name = "lblIP";
-            lblIP.Size = new Size(30, 20);
-            lblIP.TabIndex = 6;
-            lblIP.Text = "IP:";
-
-
-             
-            lblPort.Location = new Point(407, 18);
-            lblPort.Name = "lblPort";
-            lblPort.Size = new Size(53, 20);
-            lblPort.TabIndex = 7;
-            lblPort.Text = "Port:";
-
-
-            
-            ClientSize = new Size(641, 450);
-            Controls.Add(btnStartServer);
-            Controls.Add(btnStopServer);
-            Controls.Add(lblIPPort);
-            Controls.Add(listBoxLog);
-            Controls.Add(txtIP);
-            Controls.Add(txtPort);
-            Controls.Add(lblIP);
-            Controls.Add(lblPort);
-            Name = "ServerForm";
-            Text = "ServerForm";
-            ResumeLayout(false);
-            PerformLayout();
-        }
     }
 }
